8379f5b8051e7301dbdf4b7cac1272a669f0e810
gillypoo <jagill@sfu.ca>
2017-08-16 16:43:33 -0700

    All the stuff prior to 8.16.2017

--- Source Files/totalReturn_all.R
+++
@@ -1,9 +0,0 @@
-bmass <- read.csv("C:/Users/jagil/Documents/School/699/Herring Data/Source Files/Biomass_all.csv")
-names(bmass)[names(bmass) == 'ï..Year'] <- 'Year'
-ctch  <- read.csv("C:/Users/jagil/Documents/School/699/Herring Data/Source Files/Catch_all.csv")
-names(ctch)  <- c("Year", 1:49)
-library(dplyr)
-ttlrtrn <- 
-View(ttlrtrn)
-
-
